e8433a519e8a9d8ff8c094cb887c8fa255e1d9ca
Melvin Leroy <ml.lepoleswebforce3@gmail.com>
2015-09-08 10:52:23 +0200

    rien

---
+++ inscription.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+
+$dsn = 'mysql:host=localhost;dbname=admin';
+$user = 'root';
+$pass = '';
+
+$pdo = new PDO(
+	$dsn,
+	$user,
+	$pass
+);
+
+$request = $pdo->query(
+	'INSERT INTO `users`(id, email, password) VALUES (NULL,"' . $_POST['email'] . '","' . $_POST['password']. '");'
+);
+$request2= $pdo->query(
+	'SELECT email FROM users WHERE email="' . $_POST['email'] . '";'
+	);
+$result = $request2->fetchAll();
+
+
+if ( $request ) {
+	 header('Location: inscrire.php');
+	 
+} else {
+
+	//header('Location: inscription.html');
+	echo "<h2>Email déjà utilisé</h2>";
+
+}
