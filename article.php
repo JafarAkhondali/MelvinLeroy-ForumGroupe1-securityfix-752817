dfb708da3d7a6712f4ebf99cef959d01a488eb8c
Melvin <ml.lepoleswebforce3@gmail.com>
2015-09-10 12:16:37 +0200

    Modification

--- article.php
+++ article.php
@@ -15,18 +15,28 @@ $pdo = new PDO(
 	$pass
 );
 
+<<<<<<< HEAD
+
+$requestA = $pdo->query('SELECT * FROM messages WHERE id = ' . $_GET['id'] . ';');
+$resultA = $requestA->fetchAll();
+
+$requestB = $pdo->query('SELECT * FROM topics WHERE id = ' . $_GET['id'] . ';');
+$resultB = $requestB->fetchAll();
+=======
 $request = $pdo->query('SELECT * FROM messages WHERE id = ' . $_GET['id'] . ';');
 $result = $request->fetch();
 
 $request = $pdo->query('SELECT * FROM topics WHERE id = ' . $_GET['id'] . ';');
 $result = $request->fetchAll();
+>>>>>>> origin/master
 
 
 ?>
 
-<h1><?=$result[0]['title']?> le <?=$result[0]['creation']?></h1>
-<p><?=$result[0]['messages']?></p>
+<h1><?=$resultB[0]['title']?> le <?=$resultB[0]['creation']?></h1>
+<p><?=$resultA[0]['message']?></p>
 <a href="topics_liste.php">Précédent</a>
+<a href="postmessage.html">Créer un message</a>
 
 </body>
 </html>
