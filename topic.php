452b7caec5bee73f94a460f90509bb1939fd895c
Skyjoy <julien.fayollat@gmail.com>
2015-09-11 14:22:20 +0200

    go mel

--- topic.php
+++ topic.php
@@ -68,13 +68,52 @@ $result=$request->fetchAll();
                 </div>
             </div>
             <form action="formpostmessage.php?id=<?=$_GET['id']?>" method="post">
-            <input type="submit" value="Envoyer un message">
+            <input type="submit" class="send" value="Envoyer un message">
             </form>
             <br />
             <form action="index.php" method="post">
-            <input type="submit" value="Retour au Topic">
+            <input type="submit" class="back" value="Retour au Topic">
             </form>
 </body>
+<style scoped>
+body{
+    font:16px sans-serif;
+    background: url(img/new-wallpaper-14.jpg)scroll no-repeat 0 0;
+}
+h1{
+    text-align: center;
+    border-radius:5px;
+    background: #1a89db;
+    color:#fff;
+    opacity:0.7;
+}
+th{
+    background: #1a89db;
+    opacity:0.9;
+    color:#fff;
+}
+td{
+    color:#fff;
+}
+table{
+    margin:0 auto;
+    margin-top:170px;
+}
+.send{
+    border-radius:5px;
+    background: #1a89db;
+    margin:0 auto;
+    display:block;
+    margin-left: 30%;
+}
+.back{
+       border-radius:5px;
+    background: #1a89db;
+    margin:0 auto;
+    display:block;
+    margin-left: 30%;
+}
+</style>
 </html>
 
 
